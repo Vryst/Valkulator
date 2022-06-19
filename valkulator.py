d5292573d62f83c61043a4da674cfdbebed279b0
M.Rizky A.S <107788758+Vryst@users.noreply.github.com>
2022-06-20 01:22:48 +0700

    Update valkulator.py

--- valkulator.py
+++ valkulator.py
@@ -1,21 +1,21 @@
-num1 = float(input('nomor:\n'))
-comnd = str(input('pilih salah satu +,-,x,÷:\n'))
-num2 = float(input('nomor ke-2:\n'))
+num1 = float(input('Number:\n'))
+comnd = str(input('Choose one +,-,x,÷:\n'))
+num2 = float(input('2nd number:\n'))
 h1 = float(num1 + num2)
 h2 = float(num1 - num2)
 h3 = float(num1 * num2)
 h4 = float(num1 / num2)
 
 if comnd == '+':
-    print(f"Hasil dari {float(num1)}, + ,{float(num2)} adalah = {float(h1)}")
+    print(f"The result of {float(num1)}, + ,{float(num2)} is = {float(h1)}")
 
 if comnd == '-':
-    print(f"Hasil dari {float(num1)}, - ,{float(num2)} adalah = {h2}")
+    print(f"The result of {float(num1)}, - ,{float(num2)} is = {h2}")
 
 if comnd == 'x':
-    print(f"Hasil dari {float(num1)}, x ,{float(num2)} adalah = {h3}")
+    print(f"The result of {float(num1)}, x ,{float(num2)} is = {h3}")
     
 if comnd == '÷':
-    print(f"Hasil dari {float(num1)}, ÷ ,{float(num2)} adalah = {h4}")
+    print(f"The result of {float(num1)}, ÷ ,{float(num2)} is = {h4}")
     
-print("\n\nProgram selesai\nRate bang\n\nProgram Finished\nRate it bro")
+print("\n\nProgram Finished")
