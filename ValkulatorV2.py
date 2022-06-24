fde7015014e67076cf6acd105ae8c8e0138858b2
M.Rizky A.S <107788758+Vryst@users.noreply.github.com>
2022-06-24 11:17:53 +0700

    Add files via upload

--- ValkulatorV2.py
+++ ValkulatorV2.py
@@ -34,14 +34,23 @@ az = 8*"←"
 ay = 60*"↑"
 at = 60*"↓"
 
+red = '\033[91m'
+green = '\033[92m'
+blue = '\033[94m'
+bold = '\033[1m'
+italics = '\033[3m'
+underline = '\033[4m'
+end = '\033[0m'
+
 a = int(input("First number : \n"))
 o = str(input(f"{ak}\n\nOperator : Tambah ( + )\n\nKurang ( - )\n\nKali ( x )\n\nBagi ( ÷ )\n\nPangkat ( ^ )\n\nAkar ( √ )\n\nSisa Bagi ( % )\n\n{ak}\nPilih satu = "))
 b = int(input("Second number : \n"))
 
 if o == '+':
-    print(hz)
+    print(green + hz + end)
+    print(italics + 'Hasilnya adalah' + end)
     s = tambah(a,b)
-    print(hy)
+    print(green + hy + end)
 elif o == '-':
     print(hz)
     kurang(a,b)
@@ -67,4 +76,4 @@ elif o == '%':
     sbagi(a,b)
     print(hy)
 
-print(f"\n\n\n\n\n{ay}\n\n{ax}↑VALKULATOR v2 FINISHED↓{az}\n\n{at}")
+print(f"{red}\n\n\n\n\n{ay}\n\n{blue}              VALKULATOR v2 FINISHED{red}\n\n{at}{end}")
